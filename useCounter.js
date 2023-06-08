0d354c8bb290e3d60f16a17b78e909590ad51e17
gabyolivas <81489163+gabyolivas@users.noreply.github.com>
2023-06-08 19:04:23 -0400

    Commit inicial

---
+++ useCounter.js
@@ -0,0 +1,26 @@
+
+import { useState } from 'react';
+
+export const useCounter = ( initialValue = 10 ) => {
+
+    const [counter, setCounter] = useState( initialValue );
+
+    const onIncrement = ( value = 1 ) => {
+        setCounter( counter + value );
+    }
+
+    const onDecrement = ( value = 1 ) => {
+        setCounter( counter - value );
+    }
+
+    const onReset = () => {
+        setCounter( initialValue );
+    }
+    
+    return {
+        counter,
+        onIncrement,
+        onDecrement,
+        onReset
+    }
+}
---
+++ useTodo.js
@@ -0,0 +1,60 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+    return JSON.parse( localStorage.getItem( 'todos' )  ) || []
+}
+
+export const useTodo = ( initialTodos = [] ) => {     
+    
+    const [ todos, dispatchTodo ] = useReducer( todoReducer , initialTodos, init );
+
+    useEffect(() => {
+        // A grabar en el local storage
+        localStorage.setItem( 'todos', JSON.stringify( todos ) || [] ); 
+      
+    }, [todos])    
+
+    const addTodo = ( todo ) => {      
+        
+        if ( todos.findIndex( t => t.description === todo.description ) > -1 )
+        {
+            alert('Elemento duplicado, no lo insertaremos :)');
+            return;        
+        }
+        
+        const action = {
+            type: '[TODO] Add',
+            payload: todo,
+        }
+
+        dispatchTodo( action );
+    }
+
+    const deleteTodo = ( id ) => {
+        const action = {
+            type: '[TODO] Delete',
+            payload: id,
+        }
+
+        dispatchTodo( action );
+    }
+
+    const completeTodo = ( id ) => {
+        const action = {
+            type: '[TODO] Complete',
+            payload: id,
+        }
+
+        dispatchTodo( action );
+    }    
+    
+    return {
+        todos,
+        todosCount: todos.length,
+        todosPendingCount: todos.filter( t => !t.done ).length,
+        addTodo,
+        deleteTodo,
+        completeTodo,
+    }
+}
